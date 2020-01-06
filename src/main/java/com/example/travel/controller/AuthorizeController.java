f3227268f5e42d6eb587ebe7bb1d77e8de41f5b7
xyz729193 <xiyz@mail.buct.edu.cn>
2020-01-06 20:41:44 +0800

    change properties

--- src/main/java/com/example/travel/controller/AuthorizeController.java
+++ src/main/java/com/example/travel/controller/AuthorizeController.java
@@ -1,9 +1,11 @@
 package com.example.travel.controller;
 
 import com.example.travel.dto.AccessTokenDTO;
+import com.example.travel.dto.GithubUser;
 import com.example.travel.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +16,27 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private  String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("ecce60431e68faaf552f");
-        accessTokenDTO.setClient_secret("538e481351ab43a20ba084d0098456c7bfbfed13");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        githubProvider.getUser(accessToken);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
         return "index";
     }
 
